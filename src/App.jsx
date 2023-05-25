f58719edfdf4e06120f17cacccc2e4b5f1662f82
stilSW <stilqn1975@gmail.com>
2023-05-25 11:24:55 +0300

    item-details-dynamic-done

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,28 @@ import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
+
+ 
+
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/author" element={<Author />} />
-        <Route path="/item-details" element={<ItemDetails />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <SkeletonTheme>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore  />} />
+          <Route path="/author/:authorId" element={<Author />} />
+          <Route path="/item-details/:nftId" element={<ItemDetails />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </SkeletonTheme>
   );
 }
 
